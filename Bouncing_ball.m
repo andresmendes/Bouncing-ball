4cd94e8743e2c5a7f1f6d2baa6a60e9ad5a665f0
André de Souza Mendes <asmgma@gmail.com>
2021-07-06 11:27:38 -0300

    Add files via upload

---
+++ Bouncing_ball.m
@@ -0,0 +1,106 @@
+%% Bouncing ball
+% Simulation and animation of a bouncing ball.
+%
+% Reference
+% https://www.mathworks.com/help/matlab/math/ode-event-location.html
+% Mark W. Reichelt and Lawrence F. Shampine, 1/3/95
+%
+%%
+
+clear ; close all ; clc
+
+%% Parameters
+
+% Video
+tf  = 30;                   % Final time                    [s]
+fR  = 30;                   % Frame rate                    [fps]
+dt  = 1/fR;                 % Time resolution               [s]
+t   = linspace(0,tf,tf*fR); % Time                          [s]
+
+%% Simulation
+
+% VERTICAL DYNAMICS
+% Hit the ground event.
+options = odeset('Events',@hit_the_ground);
+
+% Initial conditions [height speed]
+h0 = 20;            % Initial height    [m]
+v0 = 10;            % Initial speed     [m/s]
+z0 = [h0 v0];
+
+% Time span for simulation
+TSPAN = t;
+
+% Cumulative output initialization
+t_ac = [];
+h_ac = [];
+
+% One bounce per loop
+for i = 1:12
+    
+    % Integration until hit the ground
+    [tout,zout] = ode45(@(t,z) ball_vertical_dynamics(t,z),TSPAN,z0,options);
+    
+    % Update next iteration
+    % Initial conditions [position speed]
+    z0 = [0 -.9*zout(end,2)];   
+    % Time
+    TSPAN = tout(end) + t;
+    
+    % Acumulate output
+    t_ac = [t_ac ; tout];
+    h_ac = [h_ac ; zout(:,1)];
+    
+end
+
+% LONGITUDINAL DYNAMICS
+vx  = 2;            % Horizontal speed      [m/s]
+x   = vx*t_ac;      % Horizontal position   [m]
+
+%% Animation
+
+figure
+set(gcf,'Position',[50 50 1280 720]) % 720p
+% set(gcf,'Position',[50 50 854 480]) % 480p
+
+% Create and open video writer object
+v0 = VideoWriter('Bouncing_ball.avi');
+v0.Quality   = 100;
+v0.FrameRate = fR;
+open(v0);
+
+for i=1:length(t_ac)
+    
+    hold on ; grid on ; axis equal
+    set(gca,'xlim',[0 x(end)],'ylim',[0 1.1*max(h_ac)])
+    set(gca,'xtick',0:5:x(end),'ytick',0:5:1.1*max(h_ac))
+    cla 
+    plot(x(1:i) ,h_ac(1:i)  ,'b--')
+    plot(x(i)   ,h_ac(i)    ,'bo','MarkerFaceColor','b')
+    xlabel('Horizontal distance [m]');
+    ylabel('Height [m]');
+    title('Bouncing ball');
+
+    % Adding frames
+    frame = getframe(gcf);
+    writeVideo(v0,frame);
+    
+end
+
+close(v0);
+
+%% Auxiliary functions
+
+function dz = ball_vertical_dynamics(~,z)
+    % Constant
+    g = -9.81;      % Gravity [m/s]
+    % Dynamics
+    dz(1,1) = z(2);
+    dz(2,1) = g;
+end
+
+function [value,isterminal,direction] = hit_the_ground(~,y)
+    value       = y(1);     % height = 0
+    isterminal  = 1;        % stop the integration
+    direction   = -1;       % negative direction
+end
